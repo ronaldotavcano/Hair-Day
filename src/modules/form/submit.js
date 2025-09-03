63068db3116da3a6aa50506b4ee9208e00a759c9
Ronaldo T Cano <ronaldotavarescano@gmail.com>
2025-09-03 14:31:43 -0300

    Organizando dados dos horários para a API

--- src/modules/form/submit.js
+++ src/modules/form/submit.js
@@ -1,6 +1,7 @@
 import dayjs from "dayjs"
 
 const form = document.querySelector("form")
+const clientName = document.getElementById("client")
 const selectedDate = document.getElementById("date")
 
 // Data atual para formatar o input
@@ -14,4 +15,37 @@ form.onsubmit = (event) =>{
     // Previne o comportamento padrão de recarregar a página
     event.preventDefault()
     
+    try{
+        // Recuperando o nome do cliente
+        const name = clientName.value.trim()// remove espaços a mais
+        
+        if (!name){
+            return alert("Informe o nome do cliente!")
+        }
+
+        // Recupera o horário selecionado
+        const hourSelected = document.querySelector(".hour-selected")
+        
+
+        if (!hourSelected) {
+            return alert("Selecione um horário.")
+        }
+
+        // Recuperar somente a hora
+        const [hour] = hourSelected.innerText.split(":")
+        
+        // Inserir a hora na data
+        const when = dayjs(selectedDate.value).add(hour, "hour")
+
+        // Gera um ID
+        const id = new Date().getTime()
+
+        console.log({
+            id, name, when,
+        })
+
+    }catch (error){
+        alert("Não foi possível realizar o agendamento.")
+        console.log(error)
+    }
 }
